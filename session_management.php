7dc6cd3b25551408d7415c58c5b53b94b4049deb
Isabelle Delmas (Lenertz) <isabelle@delmas.us>
2019-12-12 16:38:44 -0500

    users and admins can be created and logged in, session created

--- session_management.php
+++ session_management.php
@@ -1,13 +1,10 @@
 <?php
 
-
-
-class Sessions{
     const MONTH_IN_SECONDS = 2592000;
     const HOUR_IN_SECONDS = 3600;
     
     // Starts a user session 
-    private function start_session($name, $isAdmin = false){
+    function start_session($name, $isAdmin = false){
         ini_set('session.gc_maxlifetime', 60*60*24);
         ini_set('session.save_path', '/home/session_info_antivirus');
         session_start();
@@ -16,20 +13,19 @@ class Sessions{
         $_SESSION['isAdmin'] = $isAdmin;
         $_SESSION['check'] = hash('ripemd128', $_SERVER['REMOTE_ADDR']
                 .$_SESSION['check']);
-        
     }
     
-    public function start_user($name){
+    function start_user($name){
         return start_session($name, false);
     }
     
-    public function start_admin($name){
+    function start_admin($name){
         return start_session($name, true);
     }
     
     // Make sure the user is in their session
     // if returns false the session should be terminated
-    private function validate_session(){
+    function validate_session(){
         // changes the id to prevent hijacking
         session_regenerate_id();
         // make sure a session was started
@@ -43,24 +39,24 @@ class Sessions{
         }
     }
     
-    public function validate_admin(){
+    function validate_admin(){
         if(!$_SESSION['isAdmin']){
             return false;
         }
         return validate_session();
     }
 
-    public function validate_user(){
+    function validate_user(){
         return validate_user();
     }
 
     // Ends a user session and destroys cookies
-    public function end_session(){
+    function end_session(){
         // session_start(); // BUT WHY???
         $_SESSION = array();
         setcookie(session_name(), '', time() - $MONTH_IN_SECONDS, '/');
         session_destroy();
     }
-}
-    
+
+  
 ?> 
